79135262d2738c3b71f99cbd18fe4bf54f303dcd
Alexis00002521 <00002521@uca.edu.sv>
2024-03-18 18:07:02 -0600

    send

---
+++ app/src/main/java/com/alexis/pem_app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.alexis.pem_app
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alexis.pem_app.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+        binding.name
+
+    }
+}
